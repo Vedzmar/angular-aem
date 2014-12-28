d20c61a63e4df6eea0810a9eae0ee26174314aba
Vedzmar <iVedzmar@gmail.com>
2014-12-28 22:55:28 +0300

    Skeleton project

---
+++ angular-aem/bundle/src/test/java/org/consumer/angular/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package org.consumer.angular;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
